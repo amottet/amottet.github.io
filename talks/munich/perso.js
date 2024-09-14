b6c6ce770d1ed5cceff6eafd20cd1c917f0c15e2
Antoine Mottet <antoine.mottet@tuhh.de>
2024-09-14 12:11:46 +0200

    ...

--- talks/munich/perso.js
+++ talks/munich/perso.js
@@ -4,3 +4,19 @@ $(document).ready(function() {
         $(this).css({ "position": "absolute", "left": val+'%'});
     });
 });
+
+function initializeHighlightConfig() {
+    var highlights = []
+    $('code').each(function(){
+        var numbers = $(this).data("hln");
+        var highlight_local = [];
+        if(numbers) {
+            var arrayOfNumbers = numbers.split(',');
+            for (let a in arrayOfNumbers) {
+                highlight_local.push({start: arrayOfNumbers[a], end: arrayOfNumbers[a], color:'#555'});
+            }
+        } 
+        highlights.push(highlight_local);
+    });
+    return highlights;
+}
